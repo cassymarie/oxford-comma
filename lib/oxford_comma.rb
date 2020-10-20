e74cb5e8e67dbfe365e16872dadfdc609d638ad2
cassymarie <cassy2205@gmail.com>
2020-10-20 11:27:56 -0600

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+    size = array.size
+    newString = ""
+    if size == 2
+        newString = array.join (" and ")
+    elsif size >= 3
+        inx = 0
+        while size > 1 do
+            newString += "#{array[inx]}, " 
+            size -= 1
+            inx += 1
+        end
+        newString += "and #{array[-1]}"
+    else
+        newString = array.join
+    end
+    return newString
 end
